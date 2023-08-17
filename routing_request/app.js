6ac4846b706a93119bebb7b6689ca86c4218a949
PushpendraKPal <kumarpushp1992@gmail.com>
2023-08-17 23:44:11 +0530

    read and write files

--- routing_request/app.js
+++ routing_request/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     const url = req.url;
+    const method = req.method;
+    console.log(method)
 
     if(url === '/home'){
         res.setHeader('Content-Type', 'text/html');
@@ -24,15 +27,29 @@ const server = http.createServer((req, res)=>{
         res.write('</html>');
         return res.end();
     }
-    if(url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<body><h1>Welcome to my nodeJS project!</h1></Body>');
-        res.write('</html>');
+    if(url === '/' && method === 'GET'){
+        let data = fs.readFileSync('message.txt');
+        res.setHeader('Content-Type', 'text/html')
+        res.write(`<html lang="en"><body><h2>${data}</h2><form action="/message" method="post"><input type="text" name="message"><input type="submit" value="Send"></form></body></html>`)
         return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk)=>{
+            body.push(chunk);
+        })
+        req.on('end', ()=>{
+            let parsedData = Buffer.concat(body).toString();
+            let message = parsedData.split("=")[1];
+            console.log(message);
+            fs.writeFileSync('message.txt', message);
+            res.statusCode = 302 ;
+            res.setHeader('Location', '/');
+            return res.end();
+        })
     }
 });
 
-server.listen(4000, ()=>{
-    console.log("server is listening on port 4000")
+server.listen(3000, ()=>{
+    console.log("server is listening on port 3000")
 })
