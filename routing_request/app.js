5cfdf830f7db8213947d1fc0f0272a80520dc755
PushpendraKPal <kumarpushp1992@gmail.com>
2023-08-18 18:08:37 +0530

    changed to nodejs

--- routing_request/app.js
+++ routing_request/app.js
@@ -1,19 +1,7 @@
 const http = require('http');
-const express = require('express');
+const routesHandler = require('./routes');
 
-let app = express();
-
-const server = http.createServer(app);
-
-app.use((req, res, next)=>{
-    console.log("In the Frist middleware");
-    next();
-})
-
-app.use((req, res, next)=>{
-    console.log("In the second middleware");
-    res.send('<h2>Welcome to Express!</h2>')
-})
+const server = http.createServer(routesHandler)
 
 server.listen(3000, ()=>{
     console.log("server is listening on port 3000")
